1374e8c89c1c32c6c2d923847c5bbc542731508e
Leif Battermann <leif.battermann@gmail.com>
2018-01-12 00:03:59 +0100

    added counter example

---
+++ examples/counter/README.md
@@ -0,0 +1,11 @@
+# scalm counter example
+
+This is the minimal project setup to run the counter example.
+
+To compile the program run
+
+    sbt fastOptJS::webpack
+    
+Then start a local web server and open the `index.html` in the browser.
+
+   
---
+++ examples/counter/src/main/scala/example/Main.scala
@@ -0,0 +1,30 @@
+package example
+
+import scalm.{Html, Scalm}
+import scalm.Html._
+import org.scalajs.dom.document
+
+object Main {
+  def main(args: Array[String]): Unit = Scalm.start(document.body)(init, update, view)
+
+  type Model = Int
+
+  def init: Model = 0
+
+  sealed trait Msg
+  case object Increment extends Msg
+  case object Decrement extends Msg
+
+  def update(msg: Msg, model: Model): Model =
+    msg match {
+      case Increment => model + 2
+      case Decrement => model - 1
+    }
+
+  def view(model: Model): Html[Msg] =
+    div()(
+      button(onClick(Decrement))(text("-")),
+      div()(text(model.toString)),
+      button(onClick(Increment))(text("+"))
+    )
+}
