704725dc69d2eb4c646c8311894e2b702d8288b5
Dave Smith <davesmith.00000@gmail.com>
2021-05-23 14:26:19 +0100

    Package is top level

--- src/main/scala/scalm/package.scala
+++ src/main/scala/scalm/package.scala
@@ -1,7 +1,5 @@
+package scalm
+
 /** Scalm is a user interface library inspired by Elm.
   */
-package object scalm {
-
-  def pure[Model](model: Model): (Model, Cmd[Nothing]) = (model, Cmd.Empty)
-
-}
+def pure[Model](model: Model): (Model, Cmd[Nothing]) = (model, Cmd.Empty)
