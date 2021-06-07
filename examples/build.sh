2d7409cf6f845fbb531c66f4a49bcb841bcac61f
Dave Smith <davesmith.00000@gmail.com>
2021-06-07 22:19:16 +0100

    Examples clean during the build.sh process

--- examples/build.sh
+++ examples/build.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 cd bootstrap
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd clock
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd counter
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd field
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd http
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd mario
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
 
 cd subcomponents
-sbt test fastOptJS
+sbt clean test fastOptJS
 cd ..
