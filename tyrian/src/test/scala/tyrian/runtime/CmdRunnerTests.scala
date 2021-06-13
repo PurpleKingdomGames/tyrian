e284c040ff8405ea576b589eed4c77d35aa73a80
Dave Smith <davesmith.00000@gmail.com>
2021-06-13 15:32:32 +0100

    Added a cmd runner test

--- tyrian/src/test/scala/tyrian/runtime/CmdRunnerTests.scala
+++ tyrian/src/test/scala/tyrian/runtime/CmdRunnerTests.scala
@@ -1,9 +1,26 @@
 package tyrian.runtime
 
+import tyrian.Cmd
+import tyrian.Task
+
 class CmdRunnerTests extends munit.FunSuite  {
   
-  // test("todo") {
-  //   assert(1 == 2)
-  // }
+  test("run a cmd") {
+
+    var output: Int = -1
+
+    val cmd: Cmd[Int] =
+      Cmd.RunTask[String, Int, Int](Task.Succeeded(10), (res: Either[String, Int]) => res.toOption.getOrElse(0))
+    val callback: Int => Unit = (i: Int) => {
+      output = i
+      ()
+    }
+    val async: (=> Unit) => Unit = thing => thing
+
+    val actual = 
+      CmdRunner.runCmd(cmd, callback, async)
+
+    assertEquals(output, 10)
+  }
 
 }
