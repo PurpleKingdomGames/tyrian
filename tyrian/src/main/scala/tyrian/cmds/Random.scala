d508c66c552f2e04797c8d12c4c48366c295fd15
Dave Smith <davesmith.00000@gmail.com>
2021-06-19 14:08:56 +0100

    Random Cmd (needs tests)

---
+++ tyrian/src/main/scala/tyrian/cmds/Random.scala
@@ -0,0 +1,73 @@
+package tyrian.cmds
+
+import tyrian.Cmd
+
+object Random:
+
+  private val r: scala.util.Random =
+    new scala.util.Random()
+
+  def int: Cmd[RandomValue] =
+    makeCmd(RandomValue.NextInt(r.nextInt))
+  def int(upperLimit: Int): Cmd[RandomValue] =
+    makeCmd(RandomValue.NextInt(r.nextInt(upperLimit)))
+
+  def long: Cmd[RandomValue] =
+    makeCmd(RandomValue.NextLong(r.nextLong))
+  def long(upperLimit: Long): Cmd[RandomValue] =
+    makeCmd(RandomValue.NextLong(r.nextLong(upperLimit)))
+
+  def float: Cmd[RandomValue] =
+    makeCmd(RandomValue.NextFloat(r.nextFloat))
+
+  def double: Cmd[RandomValue] =
+    makeCmd(RandomValue.NextDouble(r.nextDouble))
+
+  def alphaNumeric(length: Int): Cmd[RandomValue] =
+    makeCmd(RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString))
+
+  private def resultToMessage[A]: Either[_, A] => RandomValue = {
+    case Right(v: Int)    => RandomValue.NextInt(v)
+    case Right(v: Long)   => RandomValue.NextLong(v)
+    case Right(v: Float)  => RandomValue.NextFloat(v)
+    case Right(v: Double) => RandomValue.NextDouble(v)
+    case Right(v: String) => RandomValue.NextAlphaNumeric(v)
+    case _                => throw new Exception("Unfailable random has failed!")
+  }
+
+  private def makeCmd[A](make: => A): Cmd[RandomValue] =
+    Cmd.Run(resultToMessage) { observable =>
+      observable.onNext(make)
+      () => ()
+    }
+
+  final case class Seeded(seed: Long):
+
+    private val r: scala.util.Random =
+      new scala.util.Random(seed)
+
+    def int(seed: Long): Cmd[RandomValue] =
+      makeCmd(RandomValue.NextInt(r.nextInt))
+    def int(seed: Long, upperLimit: Int): Cmd[RandomValue] =
+      makeCmd(RandomValue.NextInt(r.nextInt(upperLimit)))
+
+    def long: Cmd[RandomValue] =
+      makeCmd(RandomValue.NextLong(r.nextLong))
+    def long(upperLimit: Long): Cmd[RandomValue] =
+      makeCmd(RandomValue.NextLong(r.nextLong(upperLimit)))
+
+    def float: Cmd[RandomValue] =
+      makeCmd(RandomValue.NextFloat(r.nextFloat))
+
+    def double: Cmd[RandomValue] =
+      makeCmd(RandomValue.NextDouble(r.nextDouble))
+
+    def alphaNumeric(length: Int): Cmd[RandomValue] =
+      makeCmd(RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString))
+
+enum RandomValue derives CanEqual:
+  case NextInt(value: Int) extends RandomValue
+  case NextLong(value: Long) extends RandomValue
+  case NextFloat(value: Float) extends RandomValue
+  case NextDouble(value: Double) extends RandomValue
+  case NextAlphaNumeric(value: String) extends RandomValue
