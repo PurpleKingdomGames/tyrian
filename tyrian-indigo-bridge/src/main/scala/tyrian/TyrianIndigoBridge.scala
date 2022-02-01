44b5a485370a64dd83d3779621548a81f2e82aee
Dave Smith <davesmith.00000@gmail.com>
2022-02-01 21:08:00 +0000

    Fixed #38: Bridge uses pub/sub pattern

--- tyrian-indigo-bridge/src/main/scala/tyrian/TyrianIndigoBridge.scala
+++ tyrian-indigo-bridge/src/main/scala/tyrian/TyrianIndigoBridge.scala
@@ -6,39 +6,35 @@ import tyrian.Cmd
 import tyrian.Sub
 import util.Functions
 
-trait TyrianIndigoBridge[A]:
-  lazy val bridge: TyrianIndigoBridger[A] =
-    TyrianIndigoBridge.build
-
-final class TyrianIndigoBridger[A]:
+final class TyrianIndigoBridge[A]:
 
   val eventTarget: EventTarget = new EventTarget()
 
   def send(value: A): Cmd[Nothing] =
-    TyrianIndigoBridge.send(this, None, value)
+    sendToBridge(None, value)
   def sendTo(indigoGame: IndigoGameId, value: A): Cmd[Nothing] =
-    TyrianIndigoBridge.send(this, Option(indigoGame), value)
+    sendToBridge(Option(indigoGame), value)
 
   def subscribe[B](extract: A => Option[B])(using CanEqual[B, B]): Sub[B] =
-    TyrianIndigoBridge.subscribe(this, None, extract)
+    subscribeToBridge(None, extract)
   def subscribeTo[B](indigoGame: IndigoGameId)(extract: A => Option[B])(using CanEqual[B, B]): Sub[B] =
-    TyrianIndigoBridge.subscribe(this, Option(indigoGame), extract)
+    subscribeToBridge(Option(indigoGame), extract)
 
   def subSystem: TyrianSubSystem[A] =
     TyrianSubSystem(this)
   def subSystemFor(indigoGame: IndigoGameId): TyrianSubSystem[A] =
     TyrianSubSystem(Option(indigoGame), this)
 
-object TyrianIndigoBridge:
+  private def sendToBridge(indigoGameId: Option[IndigoGameId], value: A): Cmd[Nothing] =
+    Cmd.SideEffect { () =>
+      eventTarget.dispatchEvent(TyrianIndigoBridge.BridgeToIndigo(indigoGameId, value))
+      ()
+    }
 
-  def build[A]: TyrianIndigoBridger[A] =
-    new TyrianIndigoBridger[A]()
-
-  // Essentially a copy of Sub.fromEvent where the values have been fixed.
-  def subscribe[A, B](bridge: TyrianIndigoBridger[_], indigoGameId: Option[IndigoGameId], extract: A => Option[B])(using
+  private def subscribeToBridge[B](indigoGameId: Option[IndigoGameId], extract: A => Option[B])(using
       CanEqual[B, B]
   ): Sub[B] =
-    val eventExtract: BridgeToTyrian[A] => Option[B] =
+    val eventExtract: TyrianIndigoBridge.BridgeToTyrian[A] => Option[B] =
       e =>
         indigoGameId match
           case None                       => extract(e.value)
@@ -46,40 +42,39 @@ object TyrianIndigoBridge:
           case _                          => None
 
     tyrian.Sub.ofTotalObservable[B](
-      BridgeToTyrian.EventName + bridge.hashCode,
+      TyrianIndigoBridge.BridgeToTyrian.EventName + this.hashCode,
       { observer =>
-        val listener = Functions.fun { (a: BridgeToTyrian[A]) =>
+        val listener = Functions.fun { (a: TyrianIndigoBridge.BridgeToTyrian[A]) =>
           eventExtract(a) match {
             case Some(b) => observer.onNext(b)
             case None    => ()
           }
         }
-        bridge.eventTarget.addEventListener(BridgeToTyrian.EventName, listener)
-        () => bridge.eventTarget.removeEventListener(BridgeToTyrian.EventName, listener)
+        eventTarget.addEventListener(TyrianIndigoBridge.BridgeToTyrian.EventName, listener)
+        () => eventTarget.removeEventListener(TyrianIndigoBridge.BridgeToTyrian.EventName, listener)
       }
     )
 
-  def send[A](bridge: TyrianIndigoBridger[A], indigoGameId: Option[IndigoGameId], value: A): Cmd[Nothing] =
-    Cmd.SideEffect { () =>
-      bridge.eventTarget.dispatchEvent(BridgeToIndigo(indigoGameId, value))
-      ()
-    }
+object TyrianIndigoBridge:
 
-final class BridgeToIndigo[A](val indigoGameId: Option[IndigoGameId], val value: A)
-    extends Event(BridgeToIndigo.EventName)
-object BridgeToIndigo:
-  val EventName: String = "SendToIndigo"
+  def apply[A](): TyrianIndigoBridge[A] =
+    new TyrianIndigoBridge[A]()
 
-  def unapply[A](e: BridgeToIndigo[A]): Option[(Option[IndigoGameId], A)] =
-    Some((e.indigoGameId, e.value))
+  final class BridgeToIndigo[A](val indigoGameId: Option[IndigoGameId], val value: A)
+      extends Event(BridgeToIndigo.EventName)
+  object BridgeToIndigo:
+    val EventName: String = "SendToIndigo"
 
-final class BridgeToTyrian[A](val indigoGameId: Option[IndigoGameId], val value: A)
-    extends Event(BridgeToTyrian.EventName)
-object BridgeToTyrian:
-  val EventName: String = "SendToTyrian"
+    def unapply[A](e: BridgeToIndigo[A]): Option[(Option[IndigoGameId], A)] =
+      Some((e.indigoGameId, e.value))
 
-  def unapply[A](e: BridgeToTyrian[A]): Option[(Option[IndigoGameId], A)] =
-    Some((e.indigoGameId, e.value))
+  final class BridgeToTyrian[A](val indigoGameId: Option[IndigoGameId], val value: A)
+      extends Event(BridgeToTyrian.EventName)
+  object BridgeToTyrian:
+    val EventName: String = "SendToTyrian"
+
+    def unapply[A](e: BridgeToTyrian[A]): Option[(Option[IndigoGameId], A)] =
+      Some((e.indigoGameId, e.value))
 
 opaque type IndigoGameId = String
 object IndigoGameId:
