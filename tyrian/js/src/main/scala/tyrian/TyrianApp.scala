05d81383ddc494b3be87718855a335499fb9f73d
Dave Smith <davesmith.00000@gmail.com>
2022-02-02 00:28:20 +0000

    Fixed #34: Launch mechanism similar to Indigo's

--- tyrian/js/src/main/scala/tyrian/TyrianApp.scala
+++ tyrian/js/src/main/scala/tyrian/TyrianApp.scala
@@ -1,12 +1,12 @@
 package tyrian
 
-import org.scalajs.dom.Element
+import org.scalajs.dom.document
+
+import scala.scalajs.js.annotation._
 
 trait TyrianApp[Msg, Model]:
 
-  def container: Element
-
-  def init: (Model, Cmd[Msg])
+  def init(flags: Map[String, String]): (Model, Cmd[Msg])
 
   def update(msg: Msg, model: Model): (Model, Cmd[Msg])
 
@@ -14,5 +14,13 @@ trait TyrianApp[Msg, Model]:
 
   def subscriptions(model: Model): Sub[Msg]
 
-  def main(args: Array[String]): Unit =
-    Tyrian.start(container, init, update, view, subscriptions)
+  @JSExport
+  def launch(containerId: String): Unit =
+    ready(containerId, Map[String, String]())
+
+  @JSExport
+  def launch(containerId: String, flags: scala.scalajs.js.Dictionary[String]): Unit =
+    ready(containerId, flags.toMap)
+
+  private def ready(parentElementId: String, flags: Map[String, String]): Unit =
+    Tyrian.start(document.getElementById(parentElementId), init(flags), update, view, subscriptions)
