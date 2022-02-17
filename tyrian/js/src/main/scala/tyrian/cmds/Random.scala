494b0491e38cfc0bbde14196a921869e1c4f5dee
Dave Smith <davesmith.00000@gmail.com>
2022-02-17 17:13:40 +0000

    Fixed #47: Improved Random Cmd

--- tyrian/js/src/main/scala/tyrian/cmds/Random.scala
+++ tyrian/js/src/main/scala/tyrian/cmds/Random.scala
@@ -1,76 +1,124 @@
 package tyrian.cmds
 
 import tyrian.Cmd
+import tyrian.Task
 
 /** A Cmd to generate random values.
- */
+  */
 object Random:
 
   private val r: scala.util.Random =
     new scala.util.Random()
 
-  def int: Cmd[RandomValue] =
-    makeCmd(RandomValue.NextInt(r.nextInt))
-  def int(upperLimit: Int): Cmd[RandomValue] =
-    makeCmd(RandomValue.NextInt(r.nextInt(upperLimit)))
+  def int: Cmd[RandomValue.NextInt] =
+    RandomValue.NextInt(r.nextInt).toCmd
+  def int(upperLimit: Int): Cmd[RandomValue.NextInt] =
+    RandomValue.NextInt(r.nextInt(upperLimit)).toCmd
 
-  def long: Cmd[RandomValue] =
-    makeCmd(RandomValue.NextLong(r.nextLong))
-  def long(upperLimit: Long): Cmd[RandomValue] =
-    makeCmd(RandomValue.NextLong(r.nextLong(upperLimit)))
+  def long: Cmd[RandomValue.NextLong] =
+    RandomValue.NextLong(r.nextLong).toCmd
+  def long(upperLimit: Long): Cmd[RandomValue.NextLong] =
+    RandomValue.NextLong(r.nextLong(upperLimit)).toCmd
 
-  def float: Cmd[RandomValue] =
-    makeCmd(RandomValue.NextFloat(r.nextFloat))
+  def float: Cmd[RandomValue.NextFloat] =
+    RandomValue.NextFloat(r.nextFloat).toCmd
 
-  def double: Cmd[RandomValue] =
-    makeCmd(RandomValue.NextDouble(r.nextDouble))
+  def double: Cmd[RandomValue.NextDouble] =
+    RandomValue.NextDouble(r.nextDouble).toCmd
 
-  def alphaNumeric(length: Int): Cmd[RandomValue] =
-    makeCmd(RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString))
+  def alphaNumeric(length: Int): Cmd[RandomValue.NextAlphaNumeric] =
+    RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString).toCmd
 
-  @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
-  private def resultToMessage[A]: Either[_, A] => RandomValue = {
-    case Right(v: Int)    => RandomValue.NextInt(v)
-    case Right(v: Long)   => RandomValue.NextLong(v)
-    case Right(v: Float)  => RandomValue.NextFloat(v)
-    case Right(v: Double) => RandomValue.NextDouble(v)
-    case Right(v: String) => RandomValue.NextAlphaNumeric(v)
-    case _                => throw new Exception("Unfailable random has failed!")
-  }
-
-  private def makeCmd[A](make: => A): Cmd[RandomValue] =
-    Cmd.Run(resultToMessage) { observable =>
-      observable.onNext(make)
-      () => ()
-    }
+  def shuffle[A](l: List[A]): Cmd[RandomValue.NextShuffle[A]] =
+    RandomValue.NextShuffle(r.shuffle(l)).toCmd
 
   final case class Seeded(seed: Long):
 
     private val r: scala.util.Random =
       new scala.util.Random(seed)
 
-    def int(seed: Long): Cmd[RandomValue] =
-      makeCmd(RandomValue.NextInt(r.nextInt))
-    def int(seed: Long, upperLimit: Int): Cmd[RandomValue] =
-      makeCmd(RandomValue.NextInt(r.nextInt(upperLimit)))
+    def int: Cmd[RandomValue.NextInt] =
+      RandomValue.NextInt(r.nextInt).toCmd
+    def int(upperLimit: Int): Cmd[RandomValue.NextInt] =
+      RandomValue.NextInt(r.nextInt(upperLimit)).toCmd
 
-    def long: Cmd[RandomValue] =
-      makeCmd(RandomValue.NextLong(r.nextLong))
-    def long(upperLimit: Long): Cmd[RandomValue] =
-      makeCmd(RandomValue.NextLong(r.nextLong(upperLimit)))
+    def long: Cmd[RandomValue.NextLong] =
+      RandomValue.NextLong(r.nextLong).toCmd
+    def long(upperLimit: Long): Cmd[RandomValue.NextLong] =
+      RandomValue.NextLong(r.nextLong(upperLimit)).toCmd
 
-    def float: Cmd[RandomValue] =
-      makeCmd(RandomValue.NextFloat(r.nextFloat))
+    def float: Cmd[RandomValue.NextFloat] =
+      RandomValue.NextFloat(r.nextFloat).toCmd
 
-    def double: Cmd[RandomValue] =
-      makeCmd(RandomValue.NextDouble(r.nextDouble))
+    def double: Cmd[RandomValue.NextDouble] =
+      RandomValue.NextDouble(r.nextDouble).toCmd
 
-    def alphaNumeric(length: Int): Cmd[RandomValue] =
-      makeCmd(RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString))
+    def alphaNumeric(length: Int): Cmd[RandomValue.NextAlphaNumeric] =
+      RandomValue.NextAlphaNumeric(r.alphanumeric.take(length).mkString).toCmd
+
+    def shuffle[A](l: List[A]): Cmd[RandomValue.NextShuffle[A]] =
+      RandomValue.NextShuffle(r.shuffle(l)).toCmd
+
+  end Seeded
+
+  extension (i: RandomValue.NextInt)
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextInt] =
+      val f: Either[_, Int] => RandomValue.NextInt = {
+        case Right(v: Int) => RandomValue.NextInt(v)
+        case _             => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
+
+  extension (i: RandomValue.NextLong)
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextLong] =
+      val f: Either[_, Long] => RandomValue.NextLong = {
+        case Right(v: Long) => RandomValue.NextLong(v)
+        case _              => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
+
+  extension (i: RandomValue.NextFloat)
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextFloat] =
+      val f: Either[_, Float] => RandomValue.NextFloat = {
+        case Right(v: Float) => RandomValue.NextFloat(v)
+        case _               => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
+
+  extension (i: RandomValue.NextDouble)
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextDouble] =
+      val f: Either[_, Double] => RandomValue.NextDouble = {
+        case Right(v: Double) => RandomValue.NextDouble(v)
+        case _                => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
+
+  extension (i: RandomValue.NextAlphaNumeric)
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextAlphaNumeric] =
+      val f: Either[_, String] => RandomValue.NextAlphaNumeric = {
+        case Right(v: String) => RandomValue.NextAlphaNumeric(v)
+        case _                => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
+      
+  extension [A](i: RandomValue.NextShuffle[A])
+    @SuppressWarnings(Array("scalafix:DisableSyntax.throw"))
+    def toCmd: Cmd[RandomValue.NextShuffle[A]] =
+      val f: Either[_, List[A]] => RandomValue.NextShuffle[A] = {
+        case Right(v: List[A]) => RandomValue.NextShuffle(v)
+        case _                 => throw new Exception("Unfailable random has failed!")
+      }
+      Cmd.RunTask(Task.Succeeded(i.value), f)
 
 enum RandomValue derives CanEqual:
-  case NextInt(value: Int) extends RandomValue
-  case NextLong(value: Long) extends RandomValue
-  case NextFloat(value: Float) extends RandomValue
-  case NextDouble(value: Double) extends RandomValue
+  case NextInt(value: Int)             extends RandomValue
+  case NextLong(value: Long)           extends RandomValue
+  case NextFloat(value: Float)         extends RandomValue
+  case NextDouble(value: Double)       extends RandomValue
   case NextAlphaNumeric(value: String) extends RandomValue
+  case NextShuffle[A](value: List[A])  extends RandomValue
---
+++ tyrian/js/src/test/scala/tyrian/cmds/RandomTests.scala
@@ -0,0 +1,38 @@
+package tyrian.cmds
+
+class RandomTests extends munit.FunSuite {
+
+  import tyrian.CmdUtils.*
+
+  test("NextInt") {
+    val actual = Random.Seeded(2).int(5).run.value
+    assert(clue(actual) >= 0 && actual < 5)
+  }
+
+  test("NextLong") {
+    val actual = Random.Seeded(2).long(5).run.value
+    assert(clue(actual) >= 0 && actual < 5)
+  }
+
+  test("NextFloat") {
+    val actual = Random.Seeded(2).float.run.value
+    assert(clue(actual) >= 0.0f && actual <= 1.0f)
+  }
+
+  test("NextDouble") {
+    val actual = Random.Seeded(2).double.run.value
+    assert(clue(actual) >= 0.0d && actual <= 1.0d)
+  }
+
+  test("NextAlphaNumeric") {
+    val actual = Random.Seeded(2).alphaNumeric(5).run.value
+    assert(clue(actual.length) <= 5)
+    assert(clue(actual) == "oC8rH")
+  }
+
+  test("NextShuffle") {
+    val actual = Random.Seeded(2).shuffle(List(1, 2, 3)).run.value
+    assert(clue(actual) == List(3, 1, 2))
+  }
+
+}
