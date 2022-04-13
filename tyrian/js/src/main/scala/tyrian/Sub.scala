d28754d5125142ccfbd859b3d67c3d3314b40b55
Dave Smith <davesmith.00000@gmail.com>
2022-04-13 22:57:21 +0100

    Sub impls use Sync

--- tyrian/js/src/main/scala/tyrian/Sub.scala
+++ tyrian/js/src/main/scala/tyrian/Sub.scala
@@ -1,6 +1,7 @@
 package tyrian
 
 import cats.effect.kernel.Concurrent
+import cats.effect.kernel.Sync
 import org.scalajs.dom
 import org.scalajs.dom.EventTarget
 import util.Functions
@@ -73,56 +74,56 @@ object Sub:
       Batch(subs.toList)
 
   /** A subscription that emits a msg once. Identical to timeout with a duration of 0. */
-  def emit[F[_]: Concurrent, Msg](msg: Msg): Sub[F, Msg] =
+  def emit[F[_]: Sync, Msg](msg: Msg): Sub[F, Msg] =
     timeout(FiniteDuration(0, TimeUnit.MILLISECONDS), msg, msg.toString)
 
   /** A subscription that notifies its subscribers with `msg` after a `duration`. */
-  def timeout[F[_]: Concurrent, Msg](duration: FiniteDuration, msg: Msg, id: String): Sub[F, Msg] =
+  def timeout[F[_]: Sync, Msg](duration: FiniteDuration, msg: Msg, id: String): Sub[F, Msg] =
     val task =
-      Concurrent[F].pure { (callback: Either[Throwable, Msg] => Unit) =>
+      Sync[F].delay { (callback: Either[Throwable, Msg] => Unit) =>
         val handle =
           dom.window.setTimeout(
             Functions.fun0(() => callback(Right(msg))),
             duration.toMillis.toDouble
           )
-        Concurrent[F].pure(dom.window.clearTimeout(handle))
+        Sync[F].delay(dom.window.clearTimeout(handle))
       }
 
     Observe[F, Msg, Msg](id, task, identity)
 
   /** A subscription that notifies its subscribers with `msg` after a `duration`. */
-  def timeout[F[_]: Concurrent, Msg](duration: FiniteDuration, msg: Msg): Sub[F, Msg] =
+  def timeout[F[_]: Sync, Msg](duration: FiniteDuration, msg: Msg): Sub[F, Msg] =
     timeout(duration, msg, "[tyrian-sub-every] " + duration.toString + msg.toString)
 
   /** A subscription that repeatedly notifies its subscribers with `msg` based on an `interval`. */
-  def every[F[_]: Concurrent](interval: FiniteDuration, id: String): Sub[F, js.Date] =
+  def every[F[_]: Sync](interval: FiniteDuration, id: String): Sub[F, js.Date] =
     val task =
-      Concurrent[F].pure { (callback: Either[Throwable, js.Date] => Unit) =>
+      Sync[F].delay { (callback: Either[Throwable, js.Date] => Unit) =>
         val handle =
           dom.window.setInterval(
             Functions.fun0(() => callback(Right(new js.Date()))),
             interval.toMillis.toDouble
           )
-        Concurrent[F].pure(dom.window.clearTimeout(handle))
+        Sync[F].delay(dom.window.clearTimeout(handle))
       }
 
     Observe[F, js.Date, js.Date](id, task, identity)
 
   /** A subscription that repeatedly notifies its subscribers with `msg` based on an `interval`. */
-  def every[F[_]: Concurrent](interval: FiniteDuration): Sub[F, js.Date] =
+  def every[F[_]: Sync](interval: FiniteDuration): Sub[F, js.Date] =
     every(interval, "[tyrian-sub-every] " + interval.toString)
 
   /** A subscription that emtis a `msg` based on an a JavaScript event. */
-  def fromEvent[F[_]: Concurrent, A, Msg](name: String, target: EventTarget)(extract: A => Option[Msg]): Sub[F, Msg] =
+  def fromEvent[F[_]: Sync, A, Msg](name: String, target: EventTarget)(extract: A => Option[Msg]): Sub[F, Msg] =
     val task =
-      Concurrent[F].pure { (callback: Either[Throwable, Msg] => Unit) =>
+      Sync[F].delay { (callback: Either[Throwable, Msg] => Unit) =>
         val listener = Functions.fun { (a: A) =>
           extract(a) match
             case Some(msg) => callback(Right(msg))
             case None      => ()
         }
         target.addEventListener(name, listener)
-        Concurrent[F].pure(target.removeEventListener(name, listener))
+        Sync[F].delay(target.removeEventListener(name, listener))
       }
 
     Observe[F, Msg, Msg](name + target.hashCode, task, identity)
