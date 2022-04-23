1d07ff21043ce8e241ca981f7fbbd42a8e85cdcf
Dave Smith <davesmith.00000@gmail.com>
2022-04-23 14:23:52 +0100

    Added `scalajs-java-securerandom`

--- project/Dependancies.scala
+++ project/Dependancies.scala
@@ -1,9 +1,10 @@
 object Dependancies {
 
-  val catsEffect        = "3.3.8"
-  val fs2               = "3.2.7"
-  val indigoVersion     = "0.12.1"
-  val scalajsDomVersion = "2.1.0"
-  val munitCatsEffect3  = "1.0.7"
+  val catsEffect              = "3.3.11"
+  val fs2                     = "3.2.7"
+  val indigoVersion           = "0.12.1"
+  val scalajsDomVersion       = "2.1.0"
+  val munitCatsEffect3        = "1.0.7"
+  val scalajsJavaSecureRandom = "1.0.0"
 
 }
