74cff173cb8c433521129681f75b5869d9615f7f
Dave Smith <davesmith.00000@gmail.com>
2022-05-29 21:40:59 +0100

    Indigo 0.13.0

--- project/Dependancies.scala
+++ project/Dependancies.scala
@@ -1,10 +1,10 @@
 object Dependancies {
 
-  val catsEffect              = "3.3.11"
-  val fs2                     = "3.2.7"
-  val indigoVersion           = "0.12.2-SNAPSHOT"
-  val scalajsDomVersion       = "2.1.0"
-  val munitCatsEffect3        = "1.0.7"
-  val scalajsJavaSecureRandom = "1.0.0"
+  val catsEffect        = "3.3.11"
+  val fs2               = "3.2.7"
+  val indigoVersion     = "0.13.0"
+  val scalajsDomVersion = "2.1.0"
+  val munitCatsEffect3  = "1.0.7"
+  val scalajsSnabbdom   = "0.1.0"
 
 }
