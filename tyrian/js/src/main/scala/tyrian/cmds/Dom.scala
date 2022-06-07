07d2ddefbc5004af8f170ba304ccb281bcb0d511
Dave Smith <davesmith.00000@gmail.com>
2022-06-08 00:46:08 +0100

    Fixed #106: Dom blur/focus variations with no msg

--- tyrian/js/src/main/scala/tyrian/cmds/Dom.scala
+++ tyrian/js/src/main/scala/tyrian/cmds/Dom.scala
@@ -11,13 +11,25 @@ object Dom:
   case object Success
   final case class NotFound(elementId: String)
 
-  /** Focus (highlight) on a DOM input element */
+  /** Focus (highlight) on a DOM input element, emit message on complete */
   def focus[F[_]: Async, Msg](elementId: String)(resultToMessage: Either[NotFound, Success.type] => Msg): Cmd[F, Msg] =
     affectInputElement(elementId, _.focus(), resultToMessage)
 
-  /** Blur (deselect) a DOM input element */
+  /** Focus (highlight) on a DOM input element, side effect only, use form `focus(id)(resultToMessage)` to emit a
+    * message on completion.
+    */
+  def focus[F[_]: Async, Msg](elementId: String): Cmd[F, Msg] =
+    affectInputElementNoResult(elementId, _.focus())
+
+  /** Blur (deselect) a DOM input element, emit message on complete */
   def blur[F[_]: Async, Msg](elementId: String)(resultToMessage: Either[NotFound, Success.type] => Msg): Cmd[F, Msg] =
     affectInputElement(elementId, _.blur(), resultToMessage)
+
+  /** Blur (deselect) a DOM input element, side effect only, use form `blur(id)(resultToMessage)` to emit a message on
+    * completion.
+    */
+  def blur[F[_]: Async, Msg](elementId: String): Cmd[F, Msg] =
+    affectInputElementNoResult(elementId, _.blur())
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.null"))
   private def affectInputElement[F[_]: Async, Msg](
@@ -34,3 +46,13 @@ object Dom:
         else Left(NotFound(elementId))
       }
     Cmd.Run(task, resultToMessage)
+
+  @SuppressWarnings(Array("scalafix:DisableSyntax.null"))
+  private def affectInputElementNoResult[F[_]: Async, Msg](
+      elementId: String,
+      modifier: HTMLInputElement => Unit
+  ): Cmd[F, Msg] =
+    Cmd.SideEffect {
+      val node = document.getElementById(elementId)
+      if node != null then modifier(node.asInstanceOf[HTMLInputElement])
+    }
