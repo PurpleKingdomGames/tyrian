749752b705d47535820cf89fb1b2d7c9c5934f7d
Dave Smith <davesmith00000@users.noreply.github.com>
2022-09-11 09:59:19 +0100

    Fixed #143: Extension syntax to make Cmds/Subs (#144)
    * Fixed #143: Extension syntax to make Cmds/Subs
    
    * Removed unused type param

---
+++ tyrian/js/src/main/scala/tyrian/syntax.scala
@@ -0,0 +1,23 @@
+package tyrian
+
+import cats.effect.Async
+
+object syntax:
+
+  /** Make a side effect cmd from any `F[Unit]`
+    */
+  extension [F[_]](task: F[Unit])
+    def toCmd: Cmd.SideEffect[F] =
+      Cmd.SideEffect(task)
+
+  /** Make a cmd from any `F[A]`
+    */
+  extension [F[_], A](task: F[A])
+    def toCmd: Cmd.Run[F, A, A] =
+      Cmd.Run(task)(identity)
+
+  /** Make a sub from an `fs2.Stream`
+    */
+  extension [F[_]: Async, A](stream: fs2.Stream[F, A])
+    def toSub(id: String): Sub[F, A] =
+      Sub.make(id, stream)
