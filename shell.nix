a0834bfb3efc9505d9b687145e4b013f736c32db
Gabriel Volpe <volpegabriel@gmail.com>
2022-10-04 14:31:08 +0200

    nix: replace flake-compat with builtins.getFlake

--- shell.nix
+++ shell.nix
@@ -1,15 +1,3 @@
 { shell ? "main", system ? builtins.currentSystem or "unknown-system" }:
 
-let
-  compat = (import
-    (
-      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-    )
-    { src = ./.; }
-  );
-in
-compat.defaultNix.devShells.${system}.${shell}
+(builtins.getFlake ("git+file://" + toString ./.)).devShells.${system}.${shell}
