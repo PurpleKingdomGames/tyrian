b2741ea17eb704a1aa55dbf025f8750eb33b8a6d
Arman Bilge <armanbilge@gmail.com>
2023-03-18 21:41:38 +0000

    Rollback to CE 3.4.x stable

--- project/Dependancies.scala
+++ project/Dependancies.scala
@@ -1,6 +1,6 @@
 object Dependancies {
 
-  val catsEffect        = "3.5.0-RC3"
+  val catsEffect        = "3.4.8"
   val circe             = "0.14.3"
   val fs2               = "3.4.0"
   val indigoVersion     = "0.14.0"
