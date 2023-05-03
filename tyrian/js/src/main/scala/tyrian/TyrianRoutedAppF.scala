5aa91aa0259903aa74f140d673d41ce1d91e0072
Dave Smith <davesmith.00000@gmail.com>
2023-05-03 08:22:25 +0100

    Make TyrianRoutedAppF methods private

--- tyrian/js/src/main/scala/tyrian/TyrianRoutedAppF.scala
+++ tyrian/js/src/main/scala/tyrian/TyrianRoutedAppF.scala
@@ -19,17 +19,17 @@ trait TyrianRoutedAppF[F[_]: Async, Msg, Model] extends TyrianAppF[F, Msg, Model
   def setLocationHash(newHash: String): Cmd[F, Nothing] =
     Navigation.setLocationHash(newHash)
 
-  def _init(flags: Map[String, String]): (Model, Cmd[F, Msg]) =
+  private def _init(flags: Map[String, String]): (Model, Cmd[F, Msg]) =
     val (m, cmd) = init(flags)
     (m, Navigation.getLocationHash[F, Msg](hashResultToMessage) |+| cmd)
 
-  def _update(model: Model): Msg => (Model, Cmd[F, Msg]) =
+  private def _update(model: Model): Msg => (Model, Cmd[F, Msg]) =
     msg => update(model)(msg)
 
-  def _view(model: Model): Html[Msg] =
+  private def _view(model: Model): Html[Msg] =
     view(model)
 
-  def _subscriptions(model: Model): Sub[F, Msg] =
+  private def _subscriptions(model: Model): Sub[F, Msg] =
     Navigation.onLocationHashChange[F, Msg](hashChangeToMessage) |+| subscriptions(model)
 
   override def ready(node: Element, flags: Map[String, String]): Unit =
