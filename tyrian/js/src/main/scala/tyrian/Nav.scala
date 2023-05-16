fa65c9619cdffe8b29fb502c85ef73879745c675
Dave Smith <davesmith.00000@gmail.com>
2023-05-16 07:52:08 +0100

    Nav forward and back + scaladocs

--- tyrian/js/src/main/scala/tyrian/Nav.scala
+++ tyrian/js/src/main/scala/tyrian/Nav.scala
@@ -3,14 +3,23 @@ package tyrian
 import cats.effect.kernel.Async
 import org.scalajs.dom.window
 
+/** The `Nav` object provides `Cmd`s that are mainly expected to be used in conjunction with Tyrian's frontend routing.
+  * It exposes some of the functions from the JS `Location` and `History` apis.
+  */
 object Nav:
 
-  /** Update the address bar location with a new url. Should be used in conjunction with Tyrian's frontend routing so
-    * that when your model decides to change pages, you can update the browser accordingly.
+  /** Move back one in the browser's history.
     */
-  def pushUrl[F[_]: Async](url: String): Cmd[F, Nothing] =
+  def back[F[_]: Async]: Cmd[F, Nothing] =
     Cmd.SideEffect {
-      window.history.pushState("", "", url)
+      window.history.back()
+    }
+
+  /** Move forward one in the browser's history.
+    */
+  def forward[F[_]: Async]: Cmd[F, Nothing] =
+    Cmd.SideEffect {
+      window.history.forward()
     }
 
   /** Tells the browser to navigate to a new url. Should be used in conjunction with Tyrian's frontend routing when you
@@ -19,4 +28,12 @@ object Nav:
   def loadUrl[F[_]: Async](href: String): Cmd[F, Nothing] =
     Cmd.SideEffect {
       window.location.href = href
+    }
+
+  /** Update the address bar location with a new url. Should be used in conjunction with Tyrian's frontend routing so
+    * that when your model decides to change pages, you can update the browser accordingly.
+    */
+  def pushUrl[F[_]: Async](url: String): Cmd[F, Nothing] =
+    Cmd.SideEffect {
+      window.history.pushState("", "", url)
     }
