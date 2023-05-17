edfa36fef866eebd5422d74246885fd4c56cb7ef
stevechy <stevechy@gmail.com>
2023-05-17 00:51:41 -0600

    Main method launcher #198 (#200)
    * #198 : Add a launch method that scans for data-tyrian-app container elements
    
    * #198 : Add main-launcher example
    
    * #198 : Update launch method name and signature.  Add TyrianApp companion objects.
    
    * #198 : Change TyrianAppF to take both varargs and Map.  Update flag data-attr to data-tyrian-flag.  Fixed the types to IO and ZIO in tyrian-io and tyrian-zio.

---
+++ examples/main-launcher/src/main/scala/example/CounterApp.scala
@@ -0,0 +1,46 @@
+package example
+
+import cats.effect.IO
+import tyrian.Html.*
+import tyrian.*
+
+import scala.scalajs.js.annotation.*
+import scala.util.Try
+
+object CounterApp extends TyrianApp[Msg, Model]:
+
+  def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    val initialValue: Option[Int] = for {
+      initialCounter <- flags.get("InitialCounter")
+      initialCounterInt <- Try(initialCounter.toInt).toOption
+    } yield initialCounterInt
+    (Model(initialValue.getOrElse(0)), Cmd.None)
+
+  def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
+    case Msg.Increment => (model + 1, Cmd.None)
+    case Msg.Decrement => (model - 1, Cmd.None)
+    case Msg.NoOp => (model, Cmd.None)
+
+  def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Msg.Decrement))("-"),
+      div(model.toString),
+      button(onClick(Msg.Increment))("+")
+    )
+
+  def router: Location => Msg =
+    _ => Msg.NoOp
+
+  def subscriptions(model: Model): Sub[IO, Msg] =
+    Sub.None
+
+opaque type Model = Int
+object Model:
+  def apply(value: Int): Model = value
+  
+  extension (i: Model)
+    def +(other: Int): Model = i + other
+    def -(other: Int): Model = i - other
+
+enum Msg:
+  case Increment, Decrement, NoOp
--- tyrian-zio/src/main/scala/tyrian/TyrianApp.scala
+++ tyrian-zio/src/main/scala/tyrian/TyrianApp.scala
@@ -1,6 +1,7 @@
 package tyrian
 
 import cats.effect.Async
+import cats.effect.kernel.Async
 import cats.effect.kernel.Resource
 import tyrian.TyrianAppF
 import tyrian.runtime.TyrianRuntime
@@ -20,3 +21,10 @@ trait TyrianApp[Msg, Model](using Async[Task]) extends TyrianAppF[Task, Msg, Mod
     Unsafe.unsafe { implicit unsafe =>
       runtime.unsafe.run(runnable).getOrThrowFiberFailure()
     }
+
+object TyrianApp:
+  def onLoad(appDirectory: (String, TyrianAppF[Task, _, _])*)(using Async[Task]): Unit =
+    TyrianAppF.onLoad(appDirectory: _*)
+
+  def launch(appDirectory: Map[String, TyrianAppF[Task, _, _]])(using Async[Task]): Unit =
+    TyrianAppF.launch(appDirectory)
