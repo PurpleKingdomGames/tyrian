6b03cb502ae9b9ddd81ee790637b03d31467a94a
Dave Smith <davesmith.00000@gmail.com>
2024-01-02 15:50:11 +0000

    Fixed #243: Renamed TyrianApp to TyrianZ/IOApp

--- tyrian-zio/src/main/scala/tyrian/TyrianZIOApp.scala
+++ tyrian-zio/src/main/scala/tyrian/TyrianZIOApp.scala
@@ -10,7 +10,7 @@ import scala.annotation.nowarn
 /** The TyrianApp trait can be extended to conveniently prompt you for all the methods needed for a Tyrian app, as well
   * as providing a number of standard app launching methods.
   */
-trait TyrianApp[Msg, Model](using Async[Task]) extends TyrianAppF[Task, Msg, Model]:
+trait TyrianZIOApp[Msg, Model](using Async[Task]) extends TyrianAppF[Task, Msg, Model]:
 
   private val runtime = Runtime.default
 
@@ -20,7 +20,7 @@ trait TyrianApp[Msg, Model](using Async[Task]) extends TyrianAppF[Task, Msg, Mod
       runtime.unsafe.fork(runnable)
     }
 
-object TyrianApp:
+object TyrianZIOApp:
   def onLoad(appDirectory: (String, TyrianAppF[Task, _, _])*)(using Async[Task]): Unit =
     TyrianAppF.onLoad(appDirectory: _*)
 
