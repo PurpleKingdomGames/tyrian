2ce516135833534425e6167de481b7095f01867b
Dave Smith <davesmith.00000@gmail.com>
2024-08-22 10:16:37 +0100

    OOPS: Incorrect Scala 3 version numbers in js files

--- sandbox-zio/tyrianapp.js
+++ sandbox-zio/tyrianapp.js
@@ -1,5 +1,5 @@
 import {
   TyrianApp
-} from './.js/target/scala-3.4.1/sandbox-zio-fastopt/main.js';
+} from './.js/target/scala-3.4.2/sandbox-zio-fastopt/main.js';
 
 TyrianApp.launch("myapp");
--- sandbox/tyrianapp.js
+++ sandbox/tyrianapp.js
@@ -1,6 +1,6 @@
 import {
   TyrianApp
-} from './.js/target/scala-3.4.1/sandbox-fastopt/main.js';
+} from './.js/target/scala-3.4.2/sandbox-fastopt/main.js';
 
 TyrianApp.launch(document.getElementById("myapp"), {
   "message": "Hello, Tyrian!"
