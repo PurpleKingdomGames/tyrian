dd3b9e8f6145a8d178601f5a4896292c5f027825
davesmith00000 <davesmith.00000@gmail.com>
2025-01-11 21:45:49 +0000

    Upgrade all dependencies

--- project/Dependancies.scala
+++ project/Dependancies.scala
@@ -1,17 +1,17 @@
 object Dependencies {
 
-  val catsEffect        = "3.5.4"
-  val circe             = "0.14.9"
-  val fs2               = "3.10.2"
+  val catsEffect        = "3.5.7"
+  val circe             = "0.14.10"
+  val fs2               = "3.11.0"
   val scalajsDomVersion = "2.8.0"
   val munitCatsEffect3  = "2.0.0"
   val disciplineMUnit   = "2.0.0"
   val scalajsSnabbdom   = "0.2.0-M3"
-  val zio               = "2.1.7"
+  val zio               = "2.1.14"
   val zioInteropCats    = "23.1.0.3"
   val scalaJavaTime     = "2.6.0"
   val catsLaws          = "2.12.0"
-  val http4sCirce       = "0.23.24"
+  val http4sCirce       = "0.23.30"
   val http4sDom         = "0.2.11"
-  val http4sServer      = "0.23.27"
+  val http4sServer      = "0.23.30"
 }
