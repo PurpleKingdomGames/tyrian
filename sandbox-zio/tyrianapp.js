58f626e11e2c6c004a0aa5fa83489daae483d925
davesmith00000 <davesmith.00000@gmail.com>
2025-05-05 23:49:26 +0100

    Fixed sandbox js import addresses

--- sandbox-zio/tyrianapp.js
+++ sandbox-zio/tyrianapp.js
@@ -1,3 +1,3 @@
-import { TyrianApp } from "./.js/target/scala-3.6.3/sandbox-zio-fastopt/main.js";
+import { TyrianApp } from "./.js/target/scala-3.6.4/sandbox-zio-fastopt/main.js";
 
 TyrianApp.launch("myapp");
