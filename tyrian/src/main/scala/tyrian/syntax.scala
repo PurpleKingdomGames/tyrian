b88aba84272a08292a66d64812537e92bee96dc8
davesmith00000 <davesmith.00000@gmail.com>
2025-07-13 22:07:03 +0100

    Ported over Batch type from Indigo

--- tyrian/src/main/scala/tyrian/syntax.scala
+++ tyrian/src/main/scala/tyrian/syntax.scala
@@ -26,12 +26,18 @@ object syntax:
 
   extension [M](oa: Option[Elem[M]]) def orEmpty: Elem[M] = oa.getOrElse(tyrian.Empty)
 
-  // TODO: Any of this relevant?
-  // extension [A](values: Option[A])
-  //   // def toBatch: List[A]                          = Batch.fromOption(values)
+  val ==: = Batch.==:
+  val :== = Batch.:==
+
+  // TODO: Where can some of this live?
+  extension [A](values: Option[A]) def toBatch: Batch[A] = Batch.fromOption(values)
   //   def toOutcome[F[_], Msg](error: => Throwable): Outcome[F, A, Msg] = Outcome.fromOption(values, error)
 
   //   extension [A](b: Batch[Outcome[A]]) def sequence: Outcome[Batch[A]]                 = Outcome.sequenceBatch(b)
   //   extension [A](b: NonEmptyBatch[Outcome[A]]) def sequence: Outcome[NonEmptyBatch[A]] = Outcome.sequenceNonEmptyBatch(b)
   //   extension [A](l: List[Outcome[A]]) def sequence: Outcome[List[A]]                   = Outcome.sequenceList(l)
   //   extension [A](l: NonEmptyList[Outcome[A]]) def sequence: Outcome[NonEmptyList[A]]   = Outcome.sequenceNonEmptyList(l)
+  extension [A](b: Batch[Option[A]]) def sequence: Option[Batch[A]] = Batch.sequenceOption(b)
+  // extension [A](b: NonEmptyBatch[Option[A]]) def sequence: Option[NonEmptyBatch[A]] = NonEmptyBatch.sequenceOption(b)
+  extension [A](l: List[Option[A]]) def sequence: Option[List[A]] = Batch.sequenceListOption(l)
+  // extension [A](l: NonEmptyList[Option[A]]) def sequence: Option[NonEmptyList[A]]   = NonEmptyList.sequenceOption(l)
