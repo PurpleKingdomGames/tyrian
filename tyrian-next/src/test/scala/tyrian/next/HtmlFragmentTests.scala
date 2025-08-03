ecdb382084d411aa5f402b2a153557382df958fb
davesmith00000 <davesmith.00000@gmail.com>
2025-08-03 11:49:52 +0100

    Change HtmlFrag/Root so root resolves the Html

--- tyrian-next/src/test/scala/tyrian/next/HtmlFragmentTests.scala
+++ tyrian-next/src/test/scala/tyrian/next/HtmlFragmentTests.scala
@@ -4,173 +4,8 @@ import tyrian.Html.*
 
 class HtmlFragmentTests extends munit.FunSuite {
 
-  test("merging markers") {
-
-    val markerId = MarkerId("here")
-
-    val root =
-      HtmlFragment(
-        div(id := "a")(
-          div(id := "b")(
-            Marker(markerId)
-          ),
-          div(id := "c")()
-        )
-      )
-
-    val next =
-      HtmlFragment.Insert(
-        markerId,
-        p("Hello, world!")
-      )
-
-    val actual =
-      root |+| next
-
-    val expected =
-      HtmlFragment(
-        div(id := "a")(
-          div(id := "b")(
-            Marker(markerId, p("Hello, world!"))
-          ),
-          div(id := "c")()
-        )
-      )
-
-    assertEquals(actual, expected)
-  }
-
-  test("merging markers within markers") {
-
-    val markerId  = MarkerId("here")
-    val markerId2 = MarkerId("here2")
-
-    val root =
-      HtmlFragment(
-        div(id := "a")(
-          div(id := "b")(
-            Marker(markerId, p("Hello, world!"))
-          ),
-          div(id := "c")()
-        )
-      )
-
-    val next =
-      HtmlFragment.Insert(
-        markerId,
-        p("New content"),
-        div(
-          Marker(markerId2)
-        )
-      )
-
-    val next2 =
-      HtmlFragment.Insert(
-        markerId2,
-        p("New content 2")
-      )
-
-    val actual =
-      root |+| next |+| next2
-
-    val expected =
-      HtmlFragment(
-        div(id := "a")(
-          div(id := "b")(
-            Marker(
-              markerId,
-              p("Hello, world!"),
-              p("New content"),
-              div(
-                Marker(
-                  markerId2,
-                  p("New content 2")
-                )
-              )
-            )
-          ),
-          div(id := "c")()
-        )
-      )
-
-    assertEquals(actual, expected)
-  }
-
-  test("withMarkerId - MarkUp") {
-    val markerId = MarkerId("test")
-    val actual =
-      HtmlFragment
-        .MarkUp(
-          div(id := "a")(
-            div(id := "b")(),
-            div(id := "c")()
-          )
-        )
-        .withMarkerId(markerId)
-
-    val expected =
-      HtmlFragment.Insert(
-        markerId,
-        div(id := "a")(
-          div(id := "b")(),
-          div(id := "c")()
-        )
-      )
-
-    assertEquals(actual, expected)
-  }
-
-  test("withMarkerId - Insert") {
-    val markerId = MarkerId("test")
-    val actual =
-      HtmlFragment
-        .Insert(
-          MarkerId("test 2"),
-          div(id := "a")(
-            div(id := "b")(),
-            div(id := "c")()
-          )
-        )
-        .withMarkerId(markerId)
-
-    val expected =
-      HtmlFragment.Insert(
-        markerId,
-        div(id := "a")(
-          div(id := "b")(),
-          div(id := "c")()
-        )
-      )
-
-    assertEquals(actual, expected)
-  }
-
-  test("clearMarkerId") {
-    val markerId = MarkerId("test")
-    val actual =
-      HtmlFragment
-        .Insert(
-          markerId,
-          div(id := "a")(
-            div(id := "b")(),
-            div(id := "c")()
-          )
-        )
-        .clearMarkerId
-
-    val expected =
-      HtmlFragment.MarkUp(
-        div(id := "a")(
-          div(id := "b")(),
-          div(id := "c")()
-        )
-      )
-
-    assertEquals(actual, expected)
-  }
-
-  test("addHtml") {
-    val fragment = HtmlFragment.MarkUp(
+  test("append") {
+    val fragment = HtmlFragment(
       div(id := "a")(
         div(id := "b")(),
         div(id := "c")()
@@ -182,9 +17,9 @@ class HtmlFragmentTests extends munit.FunSuite {
       span("New span")
     )
 
-    val actual = fragment.addHtml(additionalHtml)
+    val actual = fragment.append(additionalHtml)
 
-    val expected = HtmlFragment.MarkUp(
+    val expected = HtmlFragment(
       div(id := "a")(
         div(id := "b")(),
         div(id := "c")()
@@ -196,8 +31,8 @@ class HtmlFragmentTests extends munit.FunSuite {
     assertEquals(actual, expected)
   }
 
-  test("withHtml") {
-    val fragment = HtmlFragment.MarkUp(
+  test("replace") {
+    val fragment = HtmlFragment(
       div(id := "a")(
         div(id := "b")(),
         div(id := "c")()
@@ -209,9 +44,9 @@ class HtmlFragmentTests extends munit.FunSuite {
       span("New span")
     )
 
-    val actual = fragment.withHtml(newEntries)
+    val actual = fragment.replace(newEntries)
 
-    val expected = HtmlFragment.MarkUp(
+    val expected = HtmlFragment(
       p("New paragraph"),
       span("New span")
     )
@@ -220,7 +55,7 @@ class HtmlFragmentTests extends munit.FunSuite {
   }
 
   test("toHtml") {
-    val fragment = HtmlFragment.MarkUp(
+    val fragment = HtmlFragment(
       div(id := "a")(
         div(id := "b")(),
         div(id := "c")()
@@ -241,134 +76,33 @@ class HtmlFragmentTests extends munit.FunSuite {
     assertEquals(actual, expected)
   }
 
-  test("combine MarkUp + MarkUp - no markers") {
-    val fragment1 = HtmlFragment.MarkUp(
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
+  test("combine") {
+    val frag1 =
+      HtmlFragment(
+        p("a"),
+        p("b")
+      ).insert(MarkerId("1"), b("1"))
+
+    val frag2 =
+      HtmlFragment(
+        p("c")
+      ).insert(MarkerId("2"), b("2"))
+
+    val actual =
+      frag1 |+| frag2
+
+    val expected =
+      HtmlFragment(
+        Batch(
+          p("a"),
+          p("b"),
+          p("c")
+        ),
+        Map(
+          MarkerId("1") -> Batch(b("1")),
+          MarkerId("2") -> Batch(b("2"))
+        )
       )
-    )
-
-    val fragment2 = HtmlFragment.MarkUp(
-      p("New paragraph"),
-      span("New span")
-    )
-
-    val actual = fragment1 |+| fragment2
-
-    val expected = HtmlFragment.MarkUp(
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
-      ),
-      p("New paragraph"),
-      span("New span")
-    )
-
-    assertEquals(actual, expected)
-  }
-
-  test("combine MarkUp + Insert - no markers") {
-    val fragment1 = HtmlFragment.MarkUp(
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
-      )
-    )
-
-    val fragment2 = HtmlFragment.Insert(
-      MarkerId("marker"),
-      p("New paragraph"),
-      span("New span")
-    )
-
-    val actual = fragment1 |+| fragment2
-
-    val expected = HtmlFragment.MarkUp(
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
-      )
-    )
-
-    assertEquals(actual, expected)
-  }
-
-  test("combine Insert + MarkUp - no markers") {
-    val fragment1 = HtmlFragment.Insert(
-      MarkerId("marker"),
-      p("New paragraph"),
-      span("New span")
-    )
-
-    val fragment2 = HtmlFragment.MarkUp(
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
-      )
-    )
-
-    val actual = fragment1 |+| fragment2
-
-    val expected = HtmlFragment.Insert(
-      MarkerId("marker"),
-      p("New paragraph"),
-      span("New span"),
-      div(id := "a")(
-        div(id := "b")(),
-        div(id := "c")()
-      )
-    )
-
-    assertEquals(actual, expected)
-  }
-
-  test("combine Insert + Insert - no markers") {
-    val fragment1 = HtmlFragment.Insert(
-      MarkerId("marker1"),
-      p("First paragraph"),
-      span("First span")
-    )
-
-    val fragment2 = HtmlFragment.Insert(
-      MarkerId("marker2"),
-      p("Second paragraph"),
-      span("Second span")
-    )
-
-    val actual = fragment1 |+| fragment2
-
-    val expected = HtmlFragment.Insert(
-      MarkerId("marker1"),
-      p("First paragraph"),
-      span("First span")
-    )
-
-    assertEquals(actual, expected)
-  }
-
-  test("combine Insert + Insert - no markers (same marker id)") {
-    val fragment1 = HtmlFragment.Insert(
-      MarkerId("marker1"),
-      p("First paragraph"),
-      span("First span")
-    )
-
-    val fragment2 = HtmlFragment.Insert(
-      MarkerId("marker1"),
-      p("Second paragraph"),
-      span("Second span")
-    )
-
-    val actual = fragment1 |+| fragment2
-
-    val expected = HtmlFragment.Insert(
-      MarkerId("marker1"),
-      p("First paragraph"),
-      span("First span"),
-      p("Second paragraph"),
-      span("Second span")
-    )
 
     assertEquals(actual, expected)
   }
