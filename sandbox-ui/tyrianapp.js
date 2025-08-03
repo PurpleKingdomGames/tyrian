0a91c44792cebeb0a035788a0f0e85f829d2f237
davesmith00000 <davesmith.00000@gmail.com>
2025-08-03 21:51:17 +0100

    Change sandbox js links to 3.7.3

--- sandbox-ui/tyrianapp.js
+++ sandbox-ui/tyrianapp.js
@@ -1,4 +1,4 @@
-import { TyrianApp } from "./.js/target/scala-3.7.1/sandbox-ui-fastopt/main.js";
+import { TyrianApp } from "./.js/target/scala-3.7.3/sandbox-ui-fastopt/main.js";
 
 TyrianApp.launch(document.getElementById("myapp"), {
   message: "Hello, Tyrian!",
