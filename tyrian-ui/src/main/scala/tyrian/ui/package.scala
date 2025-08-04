d0c74a12810f228ac33f9264485118be2d1bee90
davesmith00000 <davesmith.00000@gmail.com>
2025-08-04 22:54:08 +0100

    Aliases for existing types

---
+++ tyrian-ui/src/main/scala/tyrian/ui/package.scala
@@ -0,0 +1,81 @@
+package tyrian.ui
+
+// Button
+
+type Button = button.Button
+val Button: button.Button.type = button.Button
+
+type ButtonTheme = button.ButtonTheme
+val ButtonTheme: button.ButtonTheme.type = button.ButtonTheme
+
+// Data Types
+
+type FlexAlignment = datatypes.FlexAlignment
+val FlexAlignment: datatypes.FlexAlignment.type = datatypes.FlexAlignment
+
+type FontSize = datatypes.FontSize
+val FontSize: datatypes.FontSize.type = datatypes.FontSize
+
+type FontWeight = datatypes.FontWeight
+val FontWeight: datatypes.FontWeight.type = datatypes.FontWeight
+
+type LayoutDirection = datatypes.LayoutDirection
+val LayoutDirection: datatypes.LayoutDirection.type = datatypes.LayoutDirection
+
+type LineHeight = datatypes.LineHeight
+val LineHeight: datatypes.LineHeight.type = datatypes.LineHeight
+
+type Ratio = datatypes.Ratio
+val Ratio: datatypes.Ratio.type = datatypes.Ratio
+
+type RGB = datatypes.RGB
+val RGB: datatypes.RGB.type = datatypes.RGB
+
+type RGBA = datatypes.RGBA
+val RGBA: datatypes.RGBA.type = datatypes.RGBA
+
+type Spacing = datatypes.Spacing
+val Spacing: datatypes.Spacing.type = datatypes.Spacing
+
+type TextAlignment = datatypes.TextAlignment
+val TextAlignment: datatypes.TextAlignment.type = datatypes.TextAlignment
+
+type TextDecoration = datatypes.TextDecoration
+val TextDecoration: datatypes.TextDecoration.type = datatypes.TextDecoration
+
+type TextStyle = datatypes.TextStyle
+val TextStyle: datatypes.TextStyle.type = datatypes.TextStyle
+
+// Layout
+
+val Column: layout.Column.type = layout.Column
+
+type Container = layout.Container
+val Container: layout.Container.type = layout.Container
+
+type Layout = layout.Layout
+val Layout: layout.Layout.type = layout.Layout
+
+val Row: layout.Row.type = layout.Row
+
+// Text
+
+type TextBlock = text.TextBlock
+val TextBlock: text.TextBlock.type = text.TextBlock
+
+type TextTheme = text.TextTheme
+val TextTheme: text.TextTheme.type = text.TextTheme
+
+type TextThemes = text.TextThemes
+val TextThemes: text.TextThemes.type = text.TextThemes
+
+type TextVariant = text.TextVariant
+val TextVariant: text.TextVariant.type = text.TextVariant
+
+// Themes
+
+type ThemeColors = theme.ThemeColors
+val ThemeColors: theme.ThemeColors.type = theme.ThemeColors
+
+type ThemeFonts = theme.ThemeFonts
+val ThemeFonts: theme.ThemeFonts.type = theme.ThemeFonts
