6fef4c77b87948051dfcbcc391eca557028750c0
davesmith00000 <davesmith.00000@gmail.com>
2025-08-16 15:41:34 +0100

    Fixed #269: Anchor links with Target attributes are respected

--- sandbox-ui/src/main/scala/example/TopNav.scala
+++ sandbox-ui/src/main/scala/example/TopNav.scala
@@ -16,7 +16,7 @@ final case class TopNav():
         ),
         Link("http://tyrian.indigoengine.io/")(
           TextBlock("Tyrian website")
-        ).withTarget(Target.Blank) // TODO: Target does nothing - something about the routing I guess.
+        ).withTarget(Target.Blank)
       ).spaceAround
     )
 
