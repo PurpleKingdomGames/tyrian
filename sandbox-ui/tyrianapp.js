b0c91ce61147dc625547b14e8c9f3e6a3ad7f927
davesmith00000 <davesmith.00000@gmail.com>
2025-11-01 11:17:02 +0000

    Update the sandbox JS link

--- sandbox-ui/tyrianapp.js
+++ sandbox-ui/tyrianapp.js
@@ -1,4 +1,4 @@
-import { TyrianApp } from "./.js/target/scala-3.7.3/sandbox-ui-fastopt/main.js";
+import { TyrianApp } from "../out/sandbox-ui/js/fastLinkJS.dest/main.js";
 
 TyrianApp.launch(document.getElementById("myapp"), {
   message: "Hello, Tyrian!",
